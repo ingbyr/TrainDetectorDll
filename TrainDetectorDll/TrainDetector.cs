e0319cb1beceb5f8ae079772abf332186b08fcda
ingbyr <ingbyr@outlook.com>
2018-08-22 11:59:31 +0800

    train net

--- TrainDetectorDll/TrainDetector.cs
+++ TrainDetectorDll/TrainDetector.cs
@@ -1,44 +1,70 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Threading;
 
 namespace TrainDetectorDll
 {
     public class TrainDetector
     {
         // darknet.exe command args
-        public string trainer;
-        public string netCfg;
-        public string netWeights;
+        public string Trainer;
+        public string NetCfg;
+        public string NetWeights;
         private string extraArgs = "-dont_show";
+
+        // dir path
+        public string TrainDataPath;
+        public string DataFilePath;
+
+        // train config
+        public int Iteration;
 
         // .data file
         public struct DataFile
         {
-            public int classes;
-            public string train;
-            public string valid;
-            public string names;
-            public string backup;
+            public int Classes;
+            public string Train;
+            public string Valid;
+            public string Names;
+            public string Backup;
         }
 
-        // dir path
-        public string trainDataPath;
-        public string dataFilePath;
+        // result data by every step
+        public struct StepResult
+        {
+            public string Iterations;
+            public string AvgLoss;
+            public string Rate;
+            public string Time;
+            public string images;
+
+            public override string ToString()
+            {
+                return "iterations: " + Iterations + ", avg los: " + AvgLoss + ", rate: " + Rate + ", time: " + Time + ", images: " + images;
+            }
+        }
+
+        // msg queue
+        public FixedSizedQueue<StepResult> MsgQueue;
+
+        public bool IsTraining = false;
 
         // process
-        Process proc;
+        private Process proc;
 
         public void startTrain()
         {
-
-            Console.WriteLine(trainer +" "+ buildArgs());
+            IsTraining = true;
+            Console.WriteLine("exec: ");
+            Console.WriteLine(Trainer + " " + buildArgs());
             proc = new Process
             {
                 StartInfo = new ProcessStartInfo
                 {
-                    FileName = trainer,
+                    FileName = Trainer,
                     Arguments = buildArgs(),
                     UseShellExecute = false,
                     RedirectStandardOutput = true,
@@ -47,58 +73,113 @@ namespace TrainDetectorDll
             };
             proc.Start();
 
-            // need a thread to parse output
-            while(!proc.StandardOutput.EndOfStream)
-            {
-                string line = proc.StandardOutput.ReadLine();
-                Console.WriteLine(line);
-            }
+            // create new thread to parse output
+            var thread = new Thread(parseTrainOutput);
+            thread.Start();
         }
 
+        private void parseTrainOutput()
+        {
+            int currentIteration = 0;
+            while (currentIteration < Iteration && !proc.StandardOutput.EndOfStream)
+            {
+                //Console.WriteLine("current iteration: " + currentIteration);
+                var line = proc.StandardOutput.ReadLine();
+                if (line.EndsWith("images"))
+                {
+                    var results = line.Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+                    //Console.WriteLine("iterations: " + results[0].Split(':')[0]);
+                    //Console.WriteLine("avg loss: " + results[1].Split(' ')[0]);
+                    //Console.WriteLine("rate: " + results[2].Split(' ')[0]);
+                    //Console.WriteLine("Time: " + results[3].Split(' ')[0]); // seconds?
+                    //Console.WriteLine("images: " + results[4].Split(' ')[0]);
+                    //Console.WriteLine();
+                    StepResult stepResult;
+                    stepResult.Iterations = results[0].Split(':')[0].TrimStart();
+                    stepResult.AvgLoss = results[1].Split(' ')[0];
+                    stepResult.Rate = results[2].Split(' ')[0];
+                    stepResult.Time = results[3].Split(' ')[0]; // seconds?
+                    stepResult.images = results[4].Split(' ')[0];
+                    MsgQueue.Enqueue(stepResult);
+                    currentIteration++;
+                }
+            }
+
+            // stop the process
+            stopProcess();
+        }
+
+    
         private string buildArgs()
         {
-            return "detector train" + " " + dataFilePath + " " + netCfg + " " + netWeights + " " + extraArgs;
+            return "detector train" + " " + DataFilePath + " " + NetCfg + " " + NetWeights + " " + extraArgs;
         }
 
+        // must be invoked first
         public void prepareData(DataFile dataFile)
         {
             // get pic path list
-            string[] files = Directory.GetFileSystemEntries(trainDataPath);
+            string[] files = Directory.GetFileSystemEntries(TrainDataPath);
             List<string> pictures = new List<String>();
             foreach (var file in files)
             {
                 string extention = Path.GetExtension(file).ToLower();
-                if(extention == ".jpg")
+                if (extention == ".jpg")
                 {
                     pictures.Add(file);
                 }
             }
 
             // save to DataFile.train text file
-            using (StreamWriter writer = new StreamWriter(dataFile.train))
+            using (StreamWriter writer = new StreamWriter(dataFile.Train))
             {
-                foreach(var pic in pictures)
+                foreach (var pic in pictures)
                 {
                     writer.WriteLine(pic);
                 }
             }
 
             // save DataFile
-            using(StreamWriter writer = new StreamWriter(dataFilePath))
+            using (StreamWriter writer = new StreamWriter(DataFilePath))
             {
-                writer.WriteLine("classes = " + dataFile.classes);
-                writer.WriteLine("train = " + dataFile.train);
-                writer.WriteLine("valid = " + dataFile.valid);
-                writer.WriteLine("names = " + dataFile.names);
-                writer.WriteLine("backup = " + dataFile.backup);
+                writer.WriteLine("classes = " + dataFile.Classes);
+                writer.WriteLine("train = " + dataFile.Train);
+                writer.WriteLine("valid = " + dataFile.Valid);
+                writer.WriteLine("names = " + dataFile.Names);
+                writer.WriteLine("backup = " + dataFile.Backup);
             }
         }
 
         public void stopProcess()
         {
-            if(!proc.HasExited)
+            IsTraining = false;
+            if (proc != null && !proc.HasExited)
             {
                 proc.Kill();
+            }
+        }
+    }
+
+    public class FixedSizedQueue<T> : ConcurrentQueue<T>
+    {
+        private readonly object syncObject = new object();
+        public int Size { get; private set; }
+
+        public FixedSizedQueue(int size)
+        {
+            Size = size;
+        }
+
+        public new void Enqueue(T obj)
+        {
+            base.Enqueue(obj);
+            lock (syncObject)
+            {
+                while (base.Count > Size)
+                {
+                    T outObj;
+                    base.TryDequeue(out outObj);
+                }
             }
         }
     }
